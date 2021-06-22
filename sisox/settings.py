cda6544d0fc2cb80d141806eb8166c506d123666
tefa0408 <ealfaroz@unsa.edu.pe>
2021-06-22 12:04:40 -0500

    Conexion mysql - localhost

--- sisox/settings.py
+++ sisox/settings.py
@@ -76,11 +76,18 @@ WSGI_APPLICATION = 'sisox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'sisox_db',   
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'',
+        'PORT':'',
+        'OPTIONS' :{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        } 
     }
 }
-
+#dj_database_url.parse('')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
