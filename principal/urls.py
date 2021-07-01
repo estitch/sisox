e62eb5028f425ef6b6e964ab7d6af8d2f52a4548
JuanCarlosOrdoñezCcoriccaza <jordonezcc@unsa.edu.pe>
2021-07-01 16:15:33 -0500

    Imgenes ensambladas mas urls y views

--- principal/urls.py
+++ principal/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns =[
-    path("",views.index,name ="index")
-    
+    path("",views.index,name ="index"),
+    path("Cliente",views.Cliente,name ="Cliente"),
+    path("Administrador",views.Administrator,name ="Administrador")
     ]
--- principal/views.py
+++ principal/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def Cliente(request):
+    return render(request, "cliente.html")
+
+def Administrator(request):
+    return render(request, "Admintrador.html")
